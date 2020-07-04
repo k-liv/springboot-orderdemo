8a1493d087086b306404d7e0d1cd1fe22cccccbc
Kostas Leivadaris <example@mail>
2020-07-04 20:46:03 +0300

    Add findByOrder repository method

--- src/main/java/eu/acme/demo/repository/OrderItemRepository.java
+++ src/main/java/eu/acme/demo/repository/OrderItemRepository.java
@@ -1,10 +1,13 @@
 package eu.acme.demo.repository;
 
+import eu.acme.demo.domain.Order;
 import eu.acme.demo.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
 
+    List<OrderItem> findByOrder(Order order);
 }

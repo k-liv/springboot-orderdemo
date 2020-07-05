9c56a2cd3ea02d421013c19212bc1f0f0e026cc7
Kostas Leivadaris <example@mail>
2020-07-06 00:00:43 +0300

    Update OrderRequest with new fields

--- src/main/java/eu/acme/demo/web/dto/OrderRequest.java
+++ src/main/java/eu/acme/demo/web/dto/OrderRequest.java
@@ -1,8 +1,13 @@
 package eu.acme.demo.web.dto;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class OrderRequest {
 
-    String clientReferenceCode;
+    private String clientReferenceCode;
+    private String description;
+    private List<OrderItem> orderItems;
 
     public String getClientReferenceCode() {
         return clientReferenceCode;
@@ -12,5 +17,50 @@ public class OrderRequest {
         this.clientReferenceCode = clientReferenceCode;
     }
 
-    //TODO: place required fields in order to create an order submitted by client
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public BigDecimal getTotalAmount() {
+        BigDecimal result = BigDecimal.ZERO;
+        for (OrderItem orderItem : orderItems) {
+            result = result.add(new BigDecimal(orderItem.getUnitPrice()));
+            result = result.multiply(new BigDecimal(orderItem.getUnits()));
+        }
+        return result;
+    }
+
+    public static class OrderItem {
+
+        private String unitPrice;
+        private int units;
+
+        public String getUnitPrice() {
+            return unitPrice;
+        }
+
+        public void setUnitPrice(String unitPrice) {
+            this.unitPrice = unitPrice;
+        }
+
+        public int getUnits() {
+            return units;
+        }
+
+        public void setUnits(int units) {
+            this.units = units;
+        }
+    }
 }

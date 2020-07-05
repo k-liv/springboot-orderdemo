386dbe040e16eedb72ee79f2ebca6689ea7f02aa
Kostas Leivadaris <example@mail>
2020-07-06 00:23:46 +0300

    Implement OrderAPI

--- src/main/java/eu/acme/demo/web/OrderAPI.java
+++ src/main/java/eu/acme/demo/web/OrderAPI.java
@@ -1,7 +1,6 @@
 package eu.acme.demo.web;
 
-import eu.acme.demo.repository.OrderItemRepository;
-import eu.acme.demo.repository.OrderRepository;
+import eu.acme.demo.service.OrderService;
 import eu.acme.demo.web.dto.OrderDto;
 import eu.acme.demo.web.dto.OrderLiteDto;
 import eu.acme.demo.web.dto.OrderRequest;
@@ -14,32 +13,31 @@ import java.util.UUID;
 @RequestMapping("/orders")
 public class OrderAPI {
 
-    private final OrderRepository orderRepository;
-    private final OrderItemRepository orderItemRepository;
+    private final OrderService orderService;
 
-    public OrderAPI(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
-        this.orderRepository = orderRepository;
-        this.orderItemRepository = orderItemRepository;
+    public OrderAPI(OrderService orderService) {
+        this.orderService = orderService;
     }
 
     @GetMapping
     public List<OrderLiteDto> fetchOrders() {
-        //TODO: fetch all orders in DB
-        return null;
+        return orderService.findAll();
     }
 
     @GetMapping("/{orderId}")
     public OrderDto fetchOrder(@PathVariable UUID orderId) {
         //TODO: fetch specific order from DB
         // if order id not exists then return an HTTP 400 (bad request) with a proper payload that contains an error code and an error message
-        return null;
+
+        return orderService.findById(orderId);
     }
 
     @PostMapping
     public OrderDto submitOrder(@RequestBody OrderRequest orderRequest) {
         //TODO: submit a new order
         // if client reference code already exist then return an HTTP 400 (bad request) with a proper payload that contains an error code and an error message
-        return null;
+
+        return  orderService.save(orderRequest);
     }
 
 }
